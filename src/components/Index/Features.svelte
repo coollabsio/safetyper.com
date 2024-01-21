ab0631de8f8ab6fcf355d274bc9e208184ca73ea
Andras Bacsai <andras.bacsai@gmail.com>
2024-01-21 20:54:31 +0100

    taco

---
+++ src/components/Index/Features.svelte
@@ -0,0 +1,332 @@
+<script>
+  import Feature from "./Feature.svelte";
+</script>
+
+<div class="text-4xl text-center font-bold">
+  Sponsors
+  <svg
+    class="w-8 h-8 inline-block text-coollabs-100"
+    viewBox="0 0 256 256"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      fill="currentColor"
+      d="M240 94c0 70-103.79 126.66-108.21 129a8 8 0 0 1-7.58 0C119.79 220.66 16 164 16 94a62.07 62.07 0 0 1 62-62c20.65 0 38.73 8.88 50 23.89C139.27 40.88 157.35 32 178 32a62.07 62.07 0 0 1 62 62"
+    />
+  </svg>
+</div>
+<div class="text-center text-gray-300">
+  Special thanks to the people who sponsoring our work.
+</div>
+<div class="text-center text-xs">
+  Would you like to see yourself here? <a
+    class="text-warning underline hover:text-white"
+    href="https://github.com/sponsors/coollabsio">Here is how.</a
+  >
+</div>
+<div class="flex gap-2 flex-wrap justify-center pt-10">
+  <a href="https://cccareers.org/?utm_source=coolify.io"
+    ><img
+      src="https://github.com/cccareers.png"
+      width="60px"
+      alt="Creating Coding Careers"
+    /></a
+  >
+  <a href="https://appwrite.io/?utm_source=coolify.io"
+    ><img
+      class="rounded-full"
+      src="https://github.com/appwrite.png"
+      width="60px"
+      alt="Appwrite"
+    /></a
+  >
+  <a href="https://bc.direct/?utm_source=coolify.io"
+    ><svg
+      version="1.1"
+      id="Calque_1"
+      xmlns="http://www.w3.org/2000/svg"
+      xmlns:xlink="http://www.w3.org/1999/xlink"
+      class="w-14 h-14"
+      viewBox="0 0 290 250"
+      xml:space="preserve"
+    >
+      <style type="text/css">
+        .st0 {
+          fill: #ffffff;
+        }
+        .st1 {
+          fill: #116dff;
+        }
+      </style>
+      <g>
+        <path
+          class="st1"
+          d="M158.6,96.4c0.4,2.3,2.9,3.7,5.1,2.9L206.9,84c19.8-7.1,31.6-27.5,27.8-48.2l0,0c-4.8-26-31.7-41.5-56.6-32.7
+			l0,0c-19.8,7.1-31.6,27.5-27.8,48.2L158.6,96.4z"
+        />
+        <path
+          class="st1"
+          d="M269.1,95.4L269.1,95.4c-16-13.7-39.6-13.7-55.7,0l-34.9,29.7c-1.8,1.5-1.8,4.3,0,5.9l34.9,29.7
+			c16,13.7,39.6,13.7,55.7,0l0,0C289.3,143.7,289.3,112.6,269.1,95.4z"
+        />
+        <path
+          class="st1"
+          d="M206.9,172.2l-43.2-15.4c-2.2-0.8-4.7,0.6-5.1,2.9l-8.3,45.1c-3.8,20.7,8,41.1,27.8,48.2l0,0
+			c24.9,8.9,51.8-6.7,56.6-32.7l0,0C238.5,199.7,226.7,179.3,206.9,172.2z"
+        />
+        <path
+          class="st1"
+          d="M128.8,159.8c-0.4-2.3-2.9-3.7-5.1-2.9l-43.2,15.4c-19.8,7.1-31.6,27.5-27.8,48.2l0,0
+			c4.8,26,31.7,41.5,56.6,32.7l0,0c19.8-7.1,31.6-27.5,27.8-48.2L128.8,159.8z"
+        />
+        <path
+          class="st1"
+          d="M108.8,131.1c1.8-1.5,1.8-4.3,0-5.9L73.9,95.4c-16-13.7-39.6-13.7-55.7,0l0,0c-20.1,17.1-20.1,48.2,0,65.4
+			l0,0c16,13.7,39.6,13.7,55.7,0L108.8,131.1z"
+        />
+        <path
+          class="st1"
+          d="M80.5,84l43.2,15.4c2.2,0.8,4.7-0.6,5.1-2.9l8.3-45.1c3.8-20.7-8-41.1-27.8-48.2l0,0
+			C84.3-5.7,57.4,9.8,52.6,35.8l0,0C48.8,56.5,60.6,76.9,80.5,84z"
+        />
+      </g>
+    </svg></a
+  >
+  <a href="https://www.quantcdn.io/?utm_source=coolify.io"
+    ><img
+      class="rounded-full"
+      src="https://github.com/quantcdn.png"
+      width="60px"
+      alt="QuantCDN"
+    /></a
+  >
+  <a href="https://360creators.com/?utm_source=coolify.io"
+    ><img
+      src="https://opencollective-production.s3.us-west-1.amazonaws.com/account-avatar/503e0953-bff7-4296-b4cc-5e36d40eecc0/icon-360creators.png"
+      width="60px"
+      alt="360Creators"
+    /></a
+  >
+  <a href="https://automaze.io/?utm_source=coolify.io"
+    ><img
+      class="rounded-full"
+      src="https://github.com/automazeio.png"
+      width="60px"
+      alt="Corentin Clichy"
+    /></a
+  >
+  <a href="https://github.com/corentinclichy"
+    ><img
+      src="https://github.com/corentinclichy.png"
+      width="60px"
+      alt="Corentin Clichy"
+    /></a
+  >
+  <a href="https://github.com/Niki2k1"
+    ><img
+      class="rounded-full"
+      src="https://github.com/Niki2k1.png"
+      width="60px"
+      alt="Niklas Lausch"
+    /></a
+  >
+  <a href="https://pixel.ao/?utm_source=coolify.io"
+    ><img
+      class="rounded-full"
+      src="https://github.com/pixelinfinito.png"
+      width="60px"
+      alt="Pixel Infinito"
+    /></a
+  >
+  <a href="https://github.com/whitesidest"
+    ><img
+      class="rounded-full"
+      src="https://avatars.githubusercontent.com/u/12365916?s=52&v=4"
+      width="60px"
+      alt="Tyler Whitesides"
+    /></a
+  >
+  <a href="https://github.com/aniftyco"
+    ><img
+      class="rounded-full"
+      src="https://github.com/aniftyco.png"
+      width="60px"
+      alt="NiftyCo"
+    /></a
+  >
+  <a href="https://github.com/iujlaki"
+    ><img
+      class="rounded-full"
+      src="https://github.com/iujlaki.png"
+      width="60px"
+      alt="Imre Ujlaki"
+    /></a
+  >
+  <a href="https://il.ly/?utm_source=coolify.io"
+    ><img
+      class="rounded-full"
+      src="https://github.com/Illyism.png"
+      width="60px"
+      alt="Ilias Ism"
+    /></a
+  >
+  <a href="https://github.com/urtho"
+    ><img
+      class="rounded-full"
+      src="https://github.com/urtho.png"
+      width="60px"
+      alt="Paweł Pierścionek"
+    /></a
+  >
+  <a href="https://github.com/monocursive"
+    ><img
+      class="rounded-full"
+      src="https://github.com/monocursive.png"
+      width="60px"
+      alt="Michael Mazurczak"
+    /></a
+  >
+</div>
+<div class=" text-center">
+  <a
+    class="text-warning underline text-xs hover:text-white"
+    href="https://github.com/sponsors/coollabsio"
+  >
+    And many more...</a
+  >
+</div>
+<div class="grid grid-cols-1 md:grid-cols-2 gap-10 gap-y-28 pt-20">
+  <Feature title="AI-powered (Bring Your Own Key)">
+    <div slot="icon">
+      <svg
+        width="512"
+        height="512"
+        class="icon"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          fill="none"
+          stroke="white"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M7 7h10a2 2 0 0 1 2 2v1l1 1v3l-1 1v3a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3l-1-1v-3l1-1V9a2 2 0 0 1 2-2zm3 9h4"
+          />
+          <circle cx="8.5" cy="11.5" r=".5" fill="#000000" />
+          <circle cx="15.5" cy="11.5" r=".5" fill="#000000" />
+          <path d="M9 7L8 3m7 4l1-4" />
+        </g>
+      </svg>
+    </div>
+    <div slot="main-text">
+      SafeTyper uses the power of OpenAI's GPT to provide you with the best
+      suggestions. It's like having a personal assistant that helps you write
+      better.
+    </div>
+  </Feature>
+  <Feature title="Privacy-focused">
+    <div slot="icon">
+      <svg
+        width="512"
+        height="512"
+        class="icon"
+        viewBox="0 0 24 24"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          fill="none"
+          stroke="white"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M7 7h10a2 2 0 0 1 2 2v1l1 1v3l-1 1v3a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-3l-1-1v-3l1-1V9a2 2 0 0 1 2-2zm3 9h4"
+          />
+          <circle cx="8.5" cy="11.5" r=".5" fill="#000000" />
+          <circle cx="15.5" cy="11.5" r=".5" fill="#000000" />
+          <path d="M9 7L8 3m7 4l1-4" />
+        </g>
+      </svg>
+    </div>
+    <div slot="main-text">
+      OpenAI <a
+        class="text-blue-500 font-bold underline"
+        href="https://help.openai.com/en/articles/5722486-how-your-data-is-used-to-improve-model-performance"
+        target="_blank"
+      >
+        does not reuse</a
+      >
+      your data to train their models. You can request to
+      <a
+        href="https://help.openai.com/en/articles/6378407-how-can-i-delete-my-account"
+        class="text-blue-500 font-bold underline"
+        target="_blank">delete your</a
+      >
+      data at any time. The ultimate solution will be self-hosted / open-source LLM's. (WIP)
+      <br /><span class="text-blue-500 font-bold"></span>
+    </div>
+  </Feature>
+
+  <Feature title="Usage based billing">
+    <div slot="icon">
+      <svg
+        width="512"
+        height="512"
+        viewBox="0 0 24 24"
+        class="icon"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <g
+          fill="none"
+          stroke="currentColor"
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+        >
+          <path
+            d="M15 11h2a2 2 0 0 1 2 2v2m0 4a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2v-6a2 2 0 0 1 2-2h4"
+          />
+          <path
+            d="M11 16a1 1 0 1 0 2 0a1 1 0 1 0-2 0m-3-5V8m.347-3.631A4 4 0 0 1 16 6M3 3l18 18"
+          />
+        </g>
+      </svg>
+    </div>
+    <div slot="main-text">
+      You only <span class="font-bold text-orange-400"
+      >pay for what you use</span
+    >. If you don't use safeTyper for a month, you don't pay for
+    that month. <span class="font-bold text-orange-400"
+      >It's that simple.</span
+    >
+    </div>
+  </Feature>
+  <Feature title="Open-source">
+    <div slot="icon">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="icon"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        fill="none"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      >
+        <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+        <rect x="3" y="4" width="18" height="12" rx="1" />
+        <path d="M7 20h10" />
+        <path d="M9 16v4" />
+        <path d="M15 16v4" />
+        <path d="M7 10h2l2 3l2 -6l1 3h3" />
+      </svg>
+    </div>
+    <div slot="main-text">
+      We plan to open-source safeTyper in the future. Stay tuned!
+    </div>
+  </Feature>
+</div>
---
+++ src/components/Instances.svelte
@@ -0,0 +1,15 @@
+<script>
+  import { onMount } from "svelte";
+  let url = "https://undead.coolify.io";
+  let instances = 0;
+  onMount(async () => {
+    const { count } = await (
+      await fetch(`${url}/instances`, {
+        headers: { "cool-api-key": "coolify" },
+      })
+    ).json();
+    instances = count + "+";
+  });
+</script>
+
+<span class="text-warning font-bold">{instances} </span>
